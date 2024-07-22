5b4375325f6a1ae17f3cc01c5294292f621ab2a4
paShaman <nikitin696@gmail.com>
2024-07-22 11:18:57 +0300

    2024-07-22

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vitePluginRequire from "vite-plugin-require";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vitePluginRequire.default()
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd(), '')};
+
+  return defineConfig({
+    base: process.env.BASE_URL,
+    plugins: [
+      vue(),
+      vitePluginRequire.default()
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
-  }
-})
+  });
+}
